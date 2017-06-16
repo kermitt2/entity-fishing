df4f4010215101c664a6921521ea5d0b5c90acd3
Luca Foppiano <luca@foppiano.org>
2017-06-16 12:13:20 +0200

    moved shared utility method for serialising to JSON lists of Sentence in the Sentence class

--- src/main/java/com/scienceminer/nerd/disambiguation/Sentence.java
+++ src/main/java/com/scienceminer/nerd/disambiguation/Sentence.java
@@ -7,6 +7,8 @@ import org.grobid.core.utilities.OffsetPosition;
 
 import com.fasterxml.jackson.core.io.*;
 
+import java.util.List;
+
 /**
  * This class represents a sentence with stand-off position to mark its boundaries in a text. 
  * 
@@ -49,5 +51,24 @@ public class Sentence {
 	public String toJSON() {
 		return "{ \"offsetStart\" : " + offsets.start + ", \"offsetEnd\" : " + offsets.end + " }";
 	}
-	
+
+	/**
+	 * Utility method to serialise to JSON a list of sentences 
+	 */
+	public static String listToJSON(List<Sentence> sentences) {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append("\"sentences\" : [ ");
+		boolean start = true;
+		for (Sentence sentence : sentences) {
+			if (start) {
+				buffer.append(sentence.toJSON());
+				start = false;
+			} else
+				buffer.append(", " + sentence.toJSON());
+		}
+		buffer.append(" ]");
+
+		return buffer.toString();
+	}
+
 }
