6c7479fcb2ab5aa0d1ffd83d330eff6fba69675a
tantikristanti <tantikristanti@gmail.com>
2018-01-12 14:41:20 +0100

    fix the language definition for the class CustomisationTest class

--- src/test/java/com/scienceminer/nerd/kb/CustomisationTest.java
+++ src/test/java/com/scienceminer/nerd/kb/CustomisationTest.java
@@ -26,7 +26,7 @@ public class CustomisationTest {
 
     @Test
     public void testCreate_Retrieve() throws Exception {
-        String createJson = "{ \"wikipedia\" : [4764461, 51499, 1014346], \"freebase\": [\"/m/0cm2xh\", \"/m/0dl4z\", \"/m/02kxg_\", \"/m/06v9th\"], \"texts\": [\"World War I (WWI or WW1 or World War One), also known as the First World War or the Great War, was a global war centred in Europe that began on 28 July 1914 and lasted until 11 November 1918.\", \"The war drew in all the world's economic great powers, which were assembled in two opposing alliances: the Allies (based on the Triple Entente of the United Kingdom, France and the Russian Empire) and the Central Powers of Germany and Austria-Hungary.\"] }";
+        String createJson = "{ \"wikipedia\" : [4764461, 51499, 1014346], \"freebase\": [\"/m/0cm2xh\", \"/m/0dl4z\", \"/m/02kxg_\", \"/m/06v9th\"],\"language\" : {\"lang\":\"en\"}, \"texts\": [\"World War I (WWI or WW1 or World War One), also known as the First World War or the Great War, was a global war centred in Europe that began on 28 July 1914 and lasted until 11 November 1918.\", \"The war drew in all the world's economic great powers, which were assembled in two opposing alliances: the Allies (based on the Triple Entente of the United Kingdom, France and the Russian Empire) and the Central Powers of Germany and Austria-Hungary.\"] }";
         boolean result = target.createCustomisation("testWW1", createJson);
 
         assertThat(result, is(true));
@@ -40,7 +40,7 @@ public class CustomisationTest {
 
     @Test
     public void testCreate_Update_Delete() {
-        String createJson = "{ \"wikipedia\" : [4764461, 51499], \"freebase\": [\"/m/0cm2xh\", \"/m/0dl4z\", \"/m/02kxg_\", \"/m/06v9th\"], \"texts\": [\"World War I (WWI or WW1 or World War One), also known as the First World War or the Great War, was a global war centred in Europe that began on 28 July 1914 and lasted until 11 November 1918.\", \"The war drew in all the world's economic great powers, which were assembled in two opposing alliances: the Allies (based on the Triple Entente of the United Kingdom, France and the Russian Empire) and the Central Powers of Germany and Austria-Hungary.\"] }";
+        String createJson = "{ \"wikipedia\" : [4764461, 51499], \"freebase\": [\"/m/0cm2xh\", \"/m/0dl4z\", \"/m/02kxg_\", \"/m/06v9th\"],\"language\" : {\"lang\":\"en\"}, \"texts\": [\"World War I (WWI or WW1 or World War One), also known as the First World War or the Great War, was a global war centred in Europe that began on 28 July 1914 and lasted until 11 November 1918.\", \"The war drew in all the world's economic great powers, which were assembled in two opposing alliances: the Allies (based on the Triple Entente of the United Kingdom, France and the Russian Empire) and the Central Powers of Germany and Austria-Hungary.\"] }";
 
         target.createCustomisation("test2WW1", createJson);
 
@@ -49,7 +49,7 @@ public class CustomisationTest {
         assertThat(theCustomisations, hasSize(1));
         assertThat(target.getCustomisation("test2WW1"), is(createJson));
 
-        String updateJson = "{ \"wikipedia\" : [4764461, 1014346], \"freebase\": [], \"texts\": [\"No, no\"], \"description\" : \"This the war\" }";
+        String updateJson = "{ \"wikipedia\" : [4764461, 1014346], \"freebase\": [],\"language\" : {\"lang\":\"en\"}, \"texts\": [\"No, no\"], \"description\" : \"This the war\" }";
         boolean result = target.updateCustomisation("testWW1", updateJson);
 
         assertThat(result, is(false));
