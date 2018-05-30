0a31fc68117eb0aebdd04f45b0d610dfe6fd5b73
tantikristanti <tantikristanti@gmail.com>
2018-05-30 14:20:52 +0200

    fix the way of features collected are saved for each entity, link to a method to predict the class, test before building the database

--- src/main/java/com/scienceminer/nerd/kb/db/NerdKidDatabase.java
+++ src/main/java/com/scienceminer/nerd/kb/db/NerdKidDatabase.java
@@ -2,20 +2,23 @@ package com.scienceminer.nerd.kb.db;
 
 import com.scienceminer.nerd.exceptions.NerdResourceException;
 import com.scienceminer.nerd.kb.Statement;
-import com.scienceminer.nerd.kb.UpperKnowledgeBase;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.record.CsvRecordInput;
 import org.fusesource.lmdbjni.Entry;
 import org.fusesource.lmdbjni.Transaction;
-import org.nerd.kid.extractor.FeatureFileExtractor;
 import org.nerd.kid.model.WikidataNERPredictor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.plaf.nimbus.State;
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A factory for creating the LMDB databases used in (N)ERD Knowlegde Base for associating Wikidata Ids with their classes.
@@ -37,20 +40,15 @@ public class NerdKidDatabase extends StringRecordDatabase<List<String>> {
         throw new UnsupportedOperationException();
     }
 
+
     /*
     * build Nerd_kid database
     * */
 
-    public void buildNerdKidDatabase(StatementDatabase statementDatabase, boolean overwrite) throws Exception{
+    public void buildNerdKidDatabase(StatementDatabase statementDatabase, boolean overwrite) throws Exception {
         // read the features from the mapper
         Map<String, List<String>> resultFeatures = this.loadFeatures();
         List<String> propertiesNoValuesMapper = this.loadFeaturesNoValue();
-
-        // size of features
-        int nbOfFeatures = resultFeatures.size() + propertiesNoValuesMapper.size();
-        System.out.println("Total size: "+nbOfFeatures);
-        double[] featureVector = new double[nbOfFeatures];
-
 
         // concatenate features with value
         List<String> propValMap = new ArrayList<>();
@@ -79,86 +77,71 @@ public class NerdKidDatabase extends StringRecordDatabase<List<String>> {
                 Entry entry = kbIterator.next();
                 byte[] key = entry.getKey();
                 byte[] value = entry.getValue();
+                String wikidataId = null;
+                String predictedClass = null;
 
                 // it's better to put the create write transaction in the try to avoid writing error bug
-                try (Transaction transaction = environment.createWriteTransaction()){
-                    String wikidataId = (String) KBEnvironment.deserialize(key);
-                    // collect the content of the statement database into a list
-                    List<Statement> statements = (List<Statement>) KBUpperEnvironment.deserialize(value);
+                try (Transaction transaction = environment.createWriteTransaction()) {
+                    // we got the statement Id, just collect the Ids begin with 'Q'
+                    String statementId = (String) KBEnvironment.deserialize(key);
+                    if (statementId.startsWith("Q")) {
+                        // we have wikidataId (the wikidata Id can be get by accessing the key's statement or the concept Id of statements)
+                        wikidataId = statementId;
+                        // collect the content of the statement database into a list
+                        List<Statement> statements = (List<Statement>) KBUpperEnvironment.deserialize(value);
 
-//                    System.out.println(wikidataId);
-//                    System.out.println(Arrays.toString(statements.toArray()));
+//                        System.out.println("Wikidata Id: " + wikidataId);
+//                        System.out.println(Arrays.toString(statements.toArray()));
 
-                    // group the list by its wikidataIds
-                    Map<String, List<Statement>> mapWikidataIdGrouped = new HashMap<>();
-                    for (Statement statement : statements) {
-                        String propertyId = statement.getPropertyId();
-                        String valueProperty = statement.getValue();
-                        if (!mapWikidataIdGrouped.containsKey(wikidataId)) { // for new wikidataId that doesn't exist yet in the map
-                            List<Statement> listGroupedByWikidataId = new ArrayList<>();
-                            listGroupedByWikidataId.add(statement);
-                            mapWikidataIdGrouped.put(wikidataId, listGroupedByWikidataId);
-                        } else { // if the wikidataId has already exist, add another statement
-                            mapWikidataIdGrouped.get(wikidataId).add(statement);
-                        }
-                    }
+                        List<String> featuresNoValueCollected = new ArrayList<>();
+                        List<String> featuresCollected = new ArrayList<>();
+                        List<Double> featureVector = new ArrayList<>();
 
-                    // iterate inside the map which has been grouped by its WikidataId
-
-                    for (Map.Entry<String, List<Statement>> mapWikiIdGrouped : mapWikidataIdGrouped.entrySet()) {
-                        String keyWikidataId = mapWikiIdGrouped.getKey();
-                        List<Statement> statementList = mapWikiIdGrouped.getValue();
-                        for (Statement statement : statementList) {
+                        for (Statement statement : statements) {
+                            // for each wikidata Id, we have some features (properties + values)
                             String prop = statement.getPropertyId();
                             String val = statement.getValue();
                             String concatenatePropVal = prop + "_" + val;
-                            System.out.println(concatenatePropVal);
 
-                            // put 1 if features of entity match with the list of features in the map, otherwise put 0
+                            // collect the features collected into the lists
+                            featuresNoValueCollected.add(prop);
+                            featuresCollected.add(concatenatePropVal);
+                        }
 
-                            // iterate in features no value
-                            int idx = 0;
-                            for (String propNoVal : propertiesNoValuesMapper) {
-                                if(propNoVal.equals(prop)){
-                                    featureVector[idx] = (double) 1;
-                                } else {
-                                    featureVector[idx] = (double) 0;
-                                }
-                                idx++;
-                            }
-
-                            // iterate in features with value
-                            for (String propVal : propValMap) {
-                                if(propVal.equals(concatenatePropVal)){
-                                    featureVector[idx] = (double) 1;
-                                } else{
-                                    featureVector[idx] = (double) 0;
-                                }
-                                idx++;
-                            }
-
-                            System.out.println("Idx: " + idx);
-                            System.out.println("Feature Vector: ");
-                            for (int i=0;i<idx;idx++){
-                                System.out.println(featureVector[i]);
+                        // iterate in features no value
+                        for (String propNoVal : propertiesNoValuesMapper) {
+                            // put 1 if features of entity's feature match with the features in the mapper, otherwise put 0
+                            if (featuresNoValueCollected.contains(propNoVal)) {
+                                featureVector.add((double) 1);
+                            } else {
+                                featureVector.add((double) 0);
                             }
                         }
-                    }
+
+                        // iterate in features with value
+                        for (String propVal : propValMap) {
+                            // put 1 if features of entity's feature match with the features in the mapper, otherwise put 0
+                            if (featuresCollected.contains(propVal)) {
+                                featureVector.add((double) 1);
+                            } else {
+                                featureVector.add((double) 0);
+                            }
+                        }
+                        // convert the features vector into array of double since Smile can just predict in this data format via method reference
+                        double[] featureVectorArr = featureVector.stream().mapToDouble(Double::doubleValue).toArray();
 
                         // do the prediction based on propertyId and valueProperty collected
-                        //WikidataNERPredictor wikidataNERPredictor = new WikidataNERPredictor();
-                        //String predictedClass = wikidataNERPredictor.predict(featureVector);
-                        //System.out.println("Wikidata Id: " + wikidataId + "; Predicted class: " + predictedClass);
-                        String predictedClass = "UNKNOWN";
-                        // store the wikidata Id and its predicted class
-                        db.put(transaction,KBEnvironment.serialize(wikidataId), KBEnvironment.serialize(predictedClass));
-                        transaction.commit();
-                    //}
-                } catch(Exception e) {
+                        WikidataNERPredictor wikidataNERPredictor = new WikidataNERPredictor();
+                        predictedClass = wikidataNERPredictor.predict(featureVectorArr);
+ //                       System.out.println("Predicted class: " + predictedClass);
+                    }
+                    db.put(transaction, KBEnvironment.serialize(wikidataId), KBEnvironment.serialize(predictedClass));
+                    transaction.commit();
+                } catch (Exception e) {
                     LOGGER.error("Error when writing the database.", e);
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             LOGGER.error("Error when reading the database.", e);
         } finally {
             if (kbIterator != null)
@@ -220,4 +203,5 @@ public class NerdKidDatabase extends StringRecordDatabase<List<String>> {
             return new ArrayList<>();
         }
     }
+
 }
