25980ca87b05be5de8e4006861949ccff192ac61
lopez <patrice.lopez@science-miner.com>
2022-11-23 18:18:59 +0100

    add different sort for candidates and entities

---
+++ src/main/java/com/scienceminer/nerd/disambiguation/util/SortCandidatesByNerdScore.java
@@ -0,0 +1,33 @@
+package com.scienceminer.nerd.disambiguation.util;
+
+import java.util.*;
+import com.scienceminer.nerd.disambiguation.NerdCandidate;
+
+public class SortCandidatesByNerdScore implements Comparator<NerdCandidate> {
+    public int compare(NerdCandidate a, NerdCandidate b) {
+        //descending order
+        int val = ((int)((b.getNerdScore() - a.getNerdScore()) * 1000));
+        //return ((int)((compareQuantity - a.selectionScore) * 1000));
+
+        if (val == 0) {
+            val = ((int)((b.getSelectionScore() - a.getSelectionScore()) * 1000));
+        }
+
+        if (val == 0) {
+            val = ((int)((b.getRelatednessScore() - a.getRelatednessScore()) * 1000));
+        }
+
+        if (val == 0) {
+            val = ((int)((b.getProb_c() - a.getProb_c()) * 1000)); 
+        }
+
+        if (val == 0) {
+            if (b.getWikipediaExternalRef() != -1)
+                val = b.getWikipediaExternalRef() - a.getWikipediaExternalRef();
+            else if (b.getWikidataId() != null) 
+                val = b.getWikidataId().compareTo(a.getWikidataId());
+        }
+
+        return val;
+    }
+}
---
+++ src/main/java/com/scienceminer/nerd/disambiguation/util/SortEntitiesBySelectionScore.java
@@ -0,0 +1,37 @@
+package com.scienceminer.nerd.disambiguation.util;
+
+import java.util.*;
+import com.scienceminer.nerd.disambiguation.NerdEntity;
+
+public class SortEntitiesBySelectionScore implements Comparator<NerdEntity> {
+    public int compare(NerdEntity a, NerdEntity b) {
+        // if we have offsets
+        int bStart = b.getOffsetStart();
+        int bEnd = b.getOffsetEnd();
+
+        int aStart = a.getOffsetStart();
+        int aEnd = a.getOffsetEnd();
+
+        if ( (aStart == bStart) && (aEnd == bEnd) ) {
+            Double bScore = new Double(b.getSelectionScore());
+            Double aScore = new Double(a.getSelectionScore());
+
+            if ((bScore != 0.0) && (aScore != 0.0) && (!bScore.equals(aScore)))
+                return aScore.compareTo(bScore);
+            else {
+                aScore = new Double(a.getProb_c());
+                bScore = new Double(b.getProb_c());
+                if (aScore != bScore)
+                    return aScore.compareTo(bScore);
+                else 
+                    return a.getSource().getName().compareTo(b.getSource().getName());
+            } 
+        } else if (aStart != bStart) {
+            return aStart - bStart;
+        } else if (aEnd != bEnd) {
+            return aEnd - bEnd;
+        } else {
+            return 0;
+        }
+    }
+}
