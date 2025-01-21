f873b34ea6d7021a18ed52dfb0c877405f29fad4
Luca Foppiano <luca@foppiano.org>
2025-01-21 23:32:44 +0100

    add NL language in other parts

--- src/main/java/com/scienceminer/nerd/utilities/WikipediaLabelIDF.java
+++ src/main/java/com/scienceminer/nerd/utilities/WikipediaLabelIDF.java
@@ -141,7 +141,7 @@ public class WikipediaLabelIDF {
         }
 
         String lang = args[0];
-        List<String> languages = Arrays.asList("en", "fr", "de", "es", "it", "ar", "zh", "ja", "ru", "pt", "fa", "uk", "sv", "bn", "hi");;
+        List<String> languages = Arrays.asList("en", "fr", "de", "es", "it", "ar", "zh", "ja", "ru", "pt", "fa", "uk", "sv", "bn", "hi", "nl");;
         if (!languages.contains(lang)) {
             System.out.println("unsupported language, must be one of " + languages.toString());
             System.exit(-1);
